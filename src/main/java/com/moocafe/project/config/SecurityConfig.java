90284cb731e30c85a324f17907c720ca76d65f8b
Park Hyun Mi <phm8568s@naver.com>
2025-05-28 15:52:48 +0900

    프로젝트 최초커밋

---
+++ src/main/java/com/moocafe/project/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.moocafe.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((auth) ->
+                        auth.requestMatchers(
+                                        "/",
+                                        "/index",
+                                        "/brand/**",
+                                        "/css/**",
+                                        "/images/**",
+                                        "/include/**",
+                                        "/js/**",
+                                        "/api/**",
+                                        "/favicon.ico").permitAll()
+                                //.requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin((form) ->
+                        form.loginPage("/brand/login")
+                                .usernameParameter("user_id")
+                                .passwordParameter("user_pw")
+                                .loginProcessingUrl("/brand/login")
+                                //   .successHandler(successHandler)
+                                .defaultSuccessUrl("/index",true)
+                                .failureUrl("/brand/login?error")
+                                .permitAll()
+                ).requestCache(cache -> cache.requestCache(new HttpSessionRequestCache()))
+                .logout((logout) ->
+                        logout.logoutUrl("/brand/logout")
+                                .logoutSuccessUrl("/index")
+                                .invalidateHttpSession(true)
+                                .deleteCookies("JSESSIONID")
+                )
+                .csrf((csrf)->csrf.disable());
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/com/moocafe/project/controller/BrandController.java
@@ -0,0 +1,12 @@
+package com.moocafe.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BrandController {
+    @GetMapping({"/","/index"})
+    public String index() {
+        return "index/index";
+    }
+}
